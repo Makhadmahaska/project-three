3b8d0317633eb2a2168689c79cc1c52a47330737
Makhadmahaska <mahadmahaska@hotmail.com>
2026-03-21 13:37:52 +0100

    Add YearFilter component

---
+++ frontend/src/components/YearFilter.tsx
@@ -0,0 +1,11 @@
+
+export default function YearFilter({ setYear }: any) {
+  return (
+    <div className="filters">
+      <button onClick={() => setYear(1)}>Year 1</button>
+      <button onClick={() => setYear(2)}>Year 2</button>
+      <button onClick={() => setYear(3)}>Year 3</button>
+      <button onClick={() => setYear(0)}>All</button>
+    </div>
+  );
+}
